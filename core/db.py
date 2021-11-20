9b368952f348df64fe942b64dd87fab68549e98d
Alberto <75672168+arocavanaclocha@users.noreply.github.com>
2021-11-20 09:58:37 +0100

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,1 +1,29 @@
-sss
+try:
+  import pandas as pd
+
+except ModuleNotFoundError as m_error:
+    print(str(m_error))
+    print('please install the required module and try again...')
+    input('press enter to exit....')
+    exit()
+
+    
+def load_database(db_file_path, compression="infer"):
+  print('----------------------------')
+  print(f'Loading database file:')
+  print(f'    > file: {db_file_path}')
+  
+  db_df = pd.read_csv(db_file_path)
+  return db_df
+
+
+def flatten_product_reviews(df): 
+  #'Id' retrieved
+  columns = ['sku', 'title', 'brand',
+            'category', 'price'
+            ]
+  
+  data_dict = df.to_dict('records')
+
+  df = pd.json_normalize(data_dict, "reviews",  columns, errors='ignore' )
+  return df
