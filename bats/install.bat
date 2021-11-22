3989513ceb1ae7746ace550b7b10b2c839a4ccca
Alberto Roca <a.roca.vanaclocha@gmail.com>
2021-11-22 15:14:32 +0100

    go

---
+++ bats/install.bat
@@ -0,0 +1,10 @@
+@echo off
+echo starting scripts...
+TITLE Installation
+py -m pip install requests
+py -m pip install bs4
+py -m pip install pandas
+py -m pip install regex
+
+ECHO All scripts have been processed.
+pause
